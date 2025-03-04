a296eab1387f7ec19cb5d9e52d97f8793e2f8f08
David Breneisen <dbreneisen@gmail.com>
2025-03-03 19:24:17 -0500

    Fix desktop packageName

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -133,7 +133,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "org.eski.menoback"
+            packageName = "MeNoBack"
             packageVersion = "1.0.0"
             licenseFile.set(file("${rootDir.absolutePath}/LICENSE"))
 
