40ea3b7fcf175f068ab8457f1e9d1174922afc72
David Breneisen <dbreneisen@gmail.com>
2025-03-17 21:29:34 -0400

    Add authorship and some styling to title

--- composeApp/src/commonMain/kotlin/org/eski/menoback/ui/game/views/ActionBarMenu.kt
+++ composeApp/src/commonMain/kotlin/org/eski/menoback/ui/game/views/ActionBarMenu.kt
@@ -1,8 +1,8 @@
 package org.eski.menoback.ui.game.views
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,6 +12,7 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.EmojiEvents
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
@@ -19,10 +20,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.eski.menoback.ui.game.vm.GameScreenViewModel
+import org.eski.ui.util.grid
+import org.eski.ui.util.grid2
 import org.eski.ui.util.grid6
 import org.eski.ui.util.gridHalf
+import org.eski.ui.views.spacer
 
 @Composable
 fun ActionBarMenu(
@@ -31,13 +36,34 @@ fun ActionBarMenu(
 
   Box(modifier = Modifier.fillMaxWidth()) {
     LeftButtons(vm, Modifier.align(alignment = Alignment.CenterStart))
-    Text(
-      modifier = Modifier.align(alignment = Alignment.Center),
-      text = "MeNoBack",
-      fontSize = 24.sp,
-      fontWeight = FontWeight.Bold,
-      color = Color.LightGray,
-    )
+    Column(Modifier.align(alignment = Alignment.Center)) {
+      Row(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
+        Icon(
+          modifier = Modifier.align(alignment = Alignment.CenterVertically).size(12.dp),
+          imageVector = Icons.Filled.Favorite, contentDescription = "",
+          tint = Color.Red
+        )
+        spacer(width = grid)
+        Text(
+          text = "MeNoBack",
+          fontSize = 24.sp,
+          color = Color.White,
+        )
+        spacer(width = grid)
+        Icon(
+          modifier = Modifier.align(alignment = Alignment.CenterVertically).size(12.dp),
+          imageVector = Icons.Filled.Favorite, contentDescription = "",
+          tint = Color.Red
+        )
+      }
+      Text(
+        modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+        text = "by David Breneisen",
+        fontSize = 12.sp,
+        color = Color.LightGray,
+      )
+    }
+
     RightButtons(vm, Modifier.align(alignment = Alignment.CenterEnd))
   }
 }
--- composeApp/src/commonMain/kotlin/org/eski/ui/views/spacer.kt
+++ composeApp/src/commonMain/kotlin/org/eski/ui/views/spacer.kt
@@ -4,12 +4,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+
+@Composable
+fun spacer(size: Dp) {
+  Spacer(modifier = Modifier.size(size)
+  )
+}
 
 @Composable
 fun spacer(height: Dp = 0.dp, width: Dp = 0.dp ) {
