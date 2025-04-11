0f7002ddfcd0a2eac562cea778f9fbc279502714
David Breneisen <dbreneisen@gmail.com>
2025-04-10 21:17:09 -0400

    Don't show game settings on post game screen

--- composeApp/src/commonMain/kotlin/org/eski/menoback/ui/game/views/GameSidebar.kt
+++ composeApp/src/commonMain/kotlin/org/eski/menoback/ui/game/views/GameSidebar.kt
@@ -5,19 +5,16 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -68,9 +65,7 @@ fun RowScope.GameSidebar(vm: GameScreenViewModel) {
     }
     spacer(height = grid)
 
-
-    // Add N-Back Level and Color Mode settings when game is not running
-    if (gameState == GameState.NotStarted || gameState == GameState.GameOver) {
+    if (gameState == GameState.NotStarted) {
       NBackSettingsPanel(vm)
       Spacer(modifier = Modifier.height(grid2))
     }
